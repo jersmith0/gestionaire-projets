b1ee95d17f6c68153aaaea39ee624eba26daf03c
jersmith0 <jerrydjakou75@gmail.comgit>
2025-07-17 17:30:08 +0100

    secind commit

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,5 @@
-export const APP_NAME = "ngProject"
-export const COMPANY_NAME = 'DevFree';
+export const APP_NAME = "PixelBloom"
+export const COMPANY_NAME = 'PixelBloom';
 export const IS_SMALL = 576;
 export const IS_MEDIUM = 768;
 export const IS_DESKTOP = 1200;
